f086ad3d13cf4003c5ad8ea9da77bc2e20c74743
soukaynaassila <assilasoukayna@gmail.com>
2026-02-05 09:36:22 +0000

    update

--- Chapitre 1 – Connexion à une base de données avec PDO/connexion.php
+++ Chapitre 1 – Connexion à une base de données avec PDO/connexion.php
@@ -11,3 +11,5 @@ try {
 } catch (PDOException $e) {
     echo "Erreur de connexion : " . $e->getMessage();
 }
+/* fax kanfeteh [http://localhost/Connecter%20une%20application%20%C3%A0%20une%20base%20de%20donn%C3%A9es(php3)/Chapitre%201%20%E2%80%93%20Connexion%20%C3%A0%20une%20base%20de%20donn%C3%A9es%20avec%20PDO/connexion.php]
+kayetele3 liya message fih "Connexion réussie à la base blogdb"*/
