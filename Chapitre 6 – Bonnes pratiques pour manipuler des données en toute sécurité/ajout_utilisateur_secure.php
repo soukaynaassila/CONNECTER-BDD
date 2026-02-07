65dad0751855ec06f26e753793241c98ac9d42ed
Soukayna Assila <soukaynaassila@gmail.com>
2026-02-07 11:47:59 +0100

    chapitre 6

---
+++ Chapitre 6 – Bonnes pratiques pour manipuler des données en toute sécurité/ajout_utilisateur_secure.php
@@ -0,0 +1,37 @@
+<?php
+require 'connexion.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    $nom = htmlspecialchars(trim($_POST['nom']));
+    $email = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL);
+
+    if (!$email) {
+        die("Email invalide !");
+    }
+
+    try {
+
+        $stmt = $pdo->prepare(
+            "INSERT INTO Utilisateur (nom, email)
+             VALUES (:nom, :email)"
+        );
+
+        $stmt->execute([
+            'nom' => $nom,
+            'email' => $email
+        ]);
+
+        echo "Utilisateur ajouté avec succès";
+
+    } catch (PDOException $e) {
+
+        file_put_contents(
+            'logs/errors.log',
+            $e->getMessage() . PHP_EOL,
+            FILE_APPEND
+        );
+
+        echo "Une erreur est survenue";
+    }
+}
