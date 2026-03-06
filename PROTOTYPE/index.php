4b534fcd7e4b14578fd023371713e016fe091db9
soukaynaassila <assilasoukayna@gmail.com>
2026-03-06 10:03:42 +0000

    prototype update

--- PROTOTYPE/index.php
+++ PROTOTYPE/index.php
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-<link rel="stylesheet" href="css/style.css">
-</head>
-<body>
-   <script src="js/script.js"></script> 
-</body>
-</html>
 <?php
 ini_set('log_errors', 1);
 ini_set('error_log', __DIR__ . '/logs/errors.log');
@@ -17,13 +8,31 @@ $article = new Article();
 $articles = $article->read();
 ?>
 
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Mini Blog</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
 <h2>Liste des articles</h2>
 
-<a href="create.php">Ajouter un article</a>
+<a href="create.php" class="btn">Ajouter un article</a>
+
 <hr>
 
 <?php foreach ($articles as $a): ?>
-    <h3><?= htmlspecialchars($a['titre']) ?></h3>
-    <p><?= nl2br(htmlspecialchars($a['contenu'])) ?></p>
-    <hr>
+
+    <div class="article">
+        <h3><?= htmlspecialchars($a['titre']) ?></h3>
+        <p><?= nl2br(htmlspecialchars($a['contenu'])) ?></p>
+    </div>
+
 <?php endforeach; ?>
+
+<script src="script.js"></script>
+
+</body>
+</html>
